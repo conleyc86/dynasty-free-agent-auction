e9107d59ca2718527b8cc2129a78292fdc77084e
conleyc86 <conleyc86@gmail.com>
2026-05-01 11:58:13 -0600

    Fix search loading spinner alignment

--- app/search.tsx
+++ app/search.tsx
@@ -42,7 +42,7 @@ export default function Search({ disabled }: { disabled?: boolean }) {
           name="search"
           id="search"
           disabled={disabled}
-          className="h-10 block w-full rounded-md border border-gray-200 pl-9 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+          className="h-10 block w-full rounded-md border border-gray-200 pl-9 pr-10 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
           placeholder="Search by name..."
           spellCheck={false}
           onChange={(e) => handleSearch(e.target.value)}
@@ -52,7 +52,7 @@ export default function Search({ disabled }: { disabled?: boolean }) {
       {isPending && (
         <div className="absolute right-0 top-0 bottom-0 flex items-center justify-center">
           <svg
-            className="animate-spin -ml-1 mr-3 h-5 w-5 text-gray-700"
+            className="h-5 w-5 animate-spin text-gray-700"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
@@ -63,7 +63,7 @@ export default function Search({ disabled }: { disabled?: boolean }) {
               cy="12"
               r="10"
               stroke="currentColor"
-              stroke-width="4"
+              strokeWidth="4"
             />
             <path
               className="opacity-75"
